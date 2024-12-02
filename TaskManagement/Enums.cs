fdc5bb92a2b39e7cad4cdda451f1d1b9c5d84f45
OlehYa <olehyakuts@gmail.com>
2024-12-02 14:56:02 +0100

    Add project files.

---
+++ TaskManagement/Enums.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagement
+{
+    [Flags]
+    public enum TaskAttributes  
+    {
+        None = 0,
+        Urgent = 1,
+        RequiresReview = 2,
+        HighRisk = 4,
+        ClientVisible = 8
+    }
+
+    public enum TaskStatus
+    {
+        New,
+        InProgress,
+        Completed,
+        Cancelled
+    }
+
+    public enum TaskPriority
+    {
+        Low,
+        Medium,
+        High
+    }
+
+    public enum TaskType
+    {
+        Development,
+        Testing,
+        Documentation
+    }
+
+    public enum UserRole
+    {
+        Developer,
+        QAEngineer
+    }
+}
